8e0ba9b79d37e069ea5c47f616e681adb0dcf914
Dennis Geldmacher <geldmacher.dennis@outlook.de>
2025-01-15 08:40:49 +0100

    Updated TYPO3 version constraints in emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.4.99',
+            'typo3' => '11.0.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
