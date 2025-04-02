712bb23de06ea18f1cee7d0afcff1e08c8e40d56
Achim Fritz <af@achimfritz.de>
2025-04-02 17:38:10 +0200

    [TASK] cleanup some code before release
    also add tests for de-slashing

--- Tests/Functional/Frontend/FrontendTest.php
+++ Tests/Functional/Frontend/FrontendTest.php
@@ -2,16 +2,17 @@
 
 declare(strict_types=1);
 
-namespace B13\DeSlash\Tests\Functional\Service;
+namespace B13\DeSlash\Tests\Functional\Frontend;
 
 /*
- * This file is part of TYPO3 CMS-extension listelements by b13.
+ * This file is part of TYPO3 CMS-extension de-slash by b13.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
  */
 
+use PHPUnit\Framework\Attributes\Test;
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
@@ -36,9 +37,7 @@ class FrontendTest extends FunctionalTestCase
         'de_slash',
     ];
 
-    /**
-     * @test
-     */
+    #[Test]
     public function callPage(): void
     {
         $this->importCSVDataSet(__DIR__ . '/../Fixtures/page.csv');
@@ -51,5 +50,36 @@ class FrontendTest extends FunctionalTestCase
         $response = $this->executeFrontendSubRequest(new InternalRequest('http://localhost/'));
         $body = (string)$response->getBody();
         self::assertStringContainsString('hello world', $body);
+    }
+
+    #[Test]
+    public function canCallSubpageWithoutSlash(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/page.csv');
+        $this->setUpFrontendRootPage(
+            1,
+            [
+                'setup' => ['EXT:de_slash/Tests/Functional/Fixtures/TypoScript/setup.typoscript'],
+            ]
+        );
+        $response = $this->executeFrontendSubRequest(new InternalRequest('http://localhost/page-1'));
+        self::assertSame(200, $response->getStatusCode());
+    }
+
+    #[Test]
+    public function callSubpageWithSlashReturnsRedirectResponse(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/page.csv');
+        $this->setUpFrontendRootPage(
+            1,
+            [
+                'setup' => ['EXT:de_slash/Tests/Functional/Fixtures/TypoScript/setup.typoscript'],
+            ]
+        );
+        $response = $this->executeFrontendSubRequest(new InternalRequest('http://localhost/page-1/'));
+        self::assertSame(301, $response->getStatusCode());
+        $headers = $response->getHeaders();
+        self::assertArrayHasKey('location', $headers);
+        self::assertSame('http://localhost/page-1', $headers['location'][0] ?? null);
     }
 }
